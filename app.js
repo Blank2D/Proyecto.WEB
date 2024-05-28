f191cd043f95b6500b286420fbf9cb999631a243
CapitanDiabetes <Rickardon64@gmail.com>
2024-05-28 18:05:30 -0400

    crud arreglao (se supone)

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 //Convierto en formato json
 app.use(express.json());
 //Configuro para que la aplicacon inicie desde el director o carpeta pagina principal
-app.use(express.static(path.join(__dirname, 'pagina_principal')));
+app.use(express.static(path.join(__dirname, 'pages')));
 
 //Recibo los valores y los envio a la tabla
 app.post('/guardar_producto',(req, res) => {
@@ -46,7 +46,7 @@ app.post('/guardar_producto',(req, res) => {
 //Ruta para mostrar las películas en el listardatos.html con metodo GET
 app.get('/productos', (req, res) => {
     //Realiza una consulta SQL para seleccionar todas las filas de la tabla "peliculas"
-    connection.query('SELECT * FROM Peliculas', (err, rows) => {
+    connection.query('SELECT * FROM Productos', (err, rows) => {
         //Maneja los errores, si los hay
         if (err) throw err;
         res.send(rows); //Aquí puedes enviar la respuesta como quieras (por ejemplo, renderizar un HTML o enviar un JSON)
@@ -55,40 +55,40 @@ app.get('/productos', (req, res) => {
 
 
 //Define una ruta DELETE en la aplicación Express para eliminar una película por su ID
-app.delete('/eliminar_pelicula/:id', (req, res) => {
+app.delete('/eliminar_producto/:id', (req, res) => {
     //Obtiene el parámetro 'id' de la URL para eliminar la pelicula en especifico
     const id = req.params.id;
     //Define la consulta SQL para eliminar una película donde el ID coincida
-    const sql = 'DELETE FROM Peliculas WHERE id = ?';
+    const sql = 'DELETE FROM Productos WHERE id = ?';
     //Ejecuta la consulta SQL, utilizando el Id que se enviara a la consulta SQL
     connection.query(sql, [id], (err, result) => {
         // Si ocurre un error durante la ejecución de la consulta, lanza una excepción
         if (err) throw err;
         // Imprime un mensaje en la consola indicando que la película fue eliminada correctamente
-        console.log('Película eliminada correctamente.');
+        console.log('Producto eliminado correctamente.');
         // Envía una respuesta HTTP 200 OK al cliente, indicando que la eliminación fue exitosa
         res.sendStatus(200); 
     });
 });
 
 
-app.post('/modificar_pelicula', (req, res) => {
+app.post('/modificar_producto', (req, res) => {
     // Desestructura los datos del cuerpo de la solicitud (req.body)
-    const { id, nombre, descripcion, fecha, categoria, precio } = req.body;
+    const { id, nombre, descripcion, categoria, precio } = req.body;
     // Consulta SQL para actualizar los datos de la película en la base de datos
-    const sql = 'UPDATE Peliculas SET nombre = ?, descripcion = ?, fecha = ?, categoria = ?, precio = ? WHERE id = ?';
+    const sql = 'UPDATE Peliculas SET nombre = ?, descripcion = ?, categoria = ?, precio = ? WHERE id = ?';
     // Ejecuta la consulta SQL
-    connection.query(sql, [nombre, descripcion, fecha, categoria, precio, id], (err, result) => {
+    connection.query(sql, [nombre, descripcion, categoria, precio, id], (err, result) => {
         if (err) {
             // Si ocurre un error, muestra un mensaje en la consola y envía una respuesta de error al cliente
-            console.error('Error al modificar la película:', err);
+            console.error('Error al modificar el producto:', err);
             res.status(500).send('Error interno del servidor');
             return;
         }
         // Si la actualización es exitosa, muestra un mensaje en la consola
-        console.log('Película modificada correctamente.');
+        console.log('Producto modificado correctamente.');
         // Redirecciona al usuario a la página de listado de películas
-        res.redirect('/listardatos.html');
+        res.redirect('/listarCRUD.html');
     });
 });
 
